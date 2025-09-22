ed554ba30ee04d1ffddb85fd5d064abec4ac2e3d
Steve Jacinto <Steve_Jacinto1@baylor.edu>
2025-09-22 18:32:30 -0500

    slowed alien speed

--- settings.py
+++ settings.py
@@ -29,7 +29,7 @@ class Settings:
         """Settings that change throughout the game."""
         self.ship_speed = 1.5
         self.bullet_speed = 3.0
-        self.alien_speed = 1.0
+        self.alien_speed = .3
 
         # 1 = right, -1 = left
         self.fleet_direction = 1
@@ -38,8 +38,9 @@ class Settings:
         self.alien_points = 50
 
     def increase_speed(self):
-        """Increase speed settings and alien point values."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
-        self.alien_speed *= self.speedup_scale
+        self.alien_speed *= (self.speedup_scale * 0.5)
+        self.fleet_drop_speed *= 1.02
         self.alien_points = int(self.alien_points * self.score_scale)
+
